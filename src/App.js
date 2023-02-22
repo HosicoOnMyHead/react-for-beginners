954ea2bc4f7cd950f47808f4696c84b04489c9be
HosicoOnMyHead <jinwoo7654@naver.com>
2023-02-22 22:37:20 +0900

    Movie App Initial version

--- src/App.js
+++ src/App.js
@@ -1,85 +1,16 @@
-import { useEffect, useState } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [inverted, setInverted] = useState(false);
-  const [converted, setConverted] = useState([]);
-  const onChange = (event) => setAmount(event.target.value);
-  const reset = () => setAmount(0);
-  const onFlip = () => {
-    reset();
-    setInverted((current) => !inverted);
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  const coinValue = (event) => {
-    const option = event.target.value;
-    const name = option.substring(0, option.indexOf(" "));
-    const unit = option.substring(
-      option.indexOf(" ", option.indexOf(" ") + 1),
-      option.lastIndexOf(" ")
-    );
-    const symbol = option.substring(
-      option.indexOf("(") + 1,
-      option.indexOf(")")
-    );
-    setConverted({
-      name,
-      unit,
-      symbol,
-    });
-  };
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={coinValue}>
-          <option key="-1">Select Option</option>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <hr />
-      <div>
-        <label id="cash">You have </label>
-        <input
-          onChange={onChange}
-          id="cash"
-          type="number"
-          value={inverted ? amount * converted.unit : amount}
-          disabled={inverted}
-        />
-        USD
-      </div>
-      <div>
-        <label id="coin">Applied {converted.name} </label>
-        <input
-          onChange={onChange}
-          id="coin"
-          type="number"
-          value={inverted ? amount : amount / converted.unit}
-          disabled={!inverted}
-        />
-        {converted.symbol}
-      </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={onFlip}>{inverted ? "Trun back" : "Invert"}</button>
-    </div>
-  );
-}
+const App = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Detail />,
+  },
+]);
 
 export default App;
